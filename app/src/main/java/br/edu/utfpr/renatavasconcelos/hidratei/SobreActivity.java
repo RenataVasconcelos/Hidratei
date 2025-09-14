e46bef90d706b4c6f45c704836e32468c12c6875
Renata <renataav.rv@gmail.com>
2025-09-14 17:23:19 -0300

    Abertura de Activity com Intent Implícita

--- app/src/main/java/br/edu/utfpr/renatavasconcelos/hidratei/SobreActivity.java
+++ app/src/main/java/br/edu/utfpr/renatavasconcelos/hidratei/SobreActivity.java
@@ -1,6 +1,10 @@
 package br.edu.utfpr.renatavasconcelos.hidratei;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +19,45 @@ public class SobreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
         setTitle(R.string.sobre);
+    }
+
+    public void abrirSiteAutoria(View view){
+        abrirSite("https://github.com/RenataVasconcelos");
+    }
+    private void abrirSite(String endereco){
+        Intent intentAbertura = new Intent(Intent.ACTION_VIEW);
+
+        intentAbertura.setData(Uri.parse(endereco));
+
+        if(intentAbertura.resolveActivity(getPackageManager()) != null ){
+            startActivity(intentAbertura);
+        }else {
+            Toast.makeText(this,
+                    R.string.nenhum_aplicativo_para_abrir_paginas_web,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void enviarEmailAutor(View view){
+        enviarEmail(new String[]{"renatavasconcelos@alunos.utfpr.edu.br"},
+                          getString(R.string.contato_pelo_aplicativo));
+
+
+    }
+
+    private void enviarEmail(String[] endereco, String assunto) {
+        Intent intentAbertura = new Intent(Intent.ACTION_SENDTO);
+
+        intentAbertura.setData(Uri.parse("mailto:"));
+        intentAbertura.putExtra(Intent.EXTRA_EMAIL, endereco);
+        intentAbertura.putExtra(Intent.EXTRA_SUBJECT, assunto);
+
+        if (intentAbertura.resolveActivity(getPackageManager()) != null) {
+            startActivity(intentAbertura);
+        } else {
+            Toast.makeText(this,
+                    R.string.nenhum_aplicativo_para_enviar_um_email,
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
